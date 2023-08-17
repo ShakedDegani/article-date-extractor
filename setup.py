8c3b5b292a6d100282bfda26526795c084787e74
ShakedDegani <shaked@webz.io>
2023-08-17 12:18:24 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r', 'utf-8') as f:
 setup(
     name='webz-article-date-extractor',
     packages=['articleDateExtractor'],
-    version='0.21.3',
+    version='0.21.4',
     author='Ran Geva',
     author_email='ran@webhose.io, yitao.sun@yahoo.com, wilson.s.shilo@gmail.com',
     url='https://github.com/Webhose/article-date-extractor',
